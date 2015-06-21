a49c806de71c536605f2028469b3b05566ac49e9
Alexander Suevalov <suevalov.work@gmail.com>
2015-06-21 22:10:55 +0300

    Get rid of handlers folders

--- app/modules/auth/index.js
+++ app/modules/auth/index.js
@@ -1,7 +1,32 @@
-import LoginHandler from './handlers/login-handler';
+import React, { PropTypes } from 'react';
+import redux from 'app-redux';
+import { connect, provide } from 'redux/react';
+import { bindActionCreators } from 'redux';
+import LoginForm from 'modules/auth/components/login-form';
+import * as AuthActions from 'modules/auth/actions/auth-actions';
 import AuthRequired from 'modules/auth/decorators/auth-required';
 
 require('./login-page.less');
+
+@provide(redux)
+@connect(({ authState }) => ({
+    loggedIn: authState.loggedIn
+}))
+class LoginHandler extends React.Component {
+
+    static propTypes = {
+        loggedIn: PropTypes.bool.isRequired,
+        dispatch: PropTypes.func.isRequired
+    };
+
+    render() {
+        let actions = bindActionCreators(AuthActions, this.props.dispatch);
+        return (
+            <LoginForm actions={actions} />
+        );
+    }
+
+}
 
 export default {
     LoginHandler: LoginHandler,
--- app/modules/todo/index.js
+++ app/modules/todo/index.js
@@ -1,20 +1,40 @@
-import React from 'react';
-import { Provider } from 'redux/react';
-import redux from 'app-redux';
-import TodoHandler from './handlers/todo-handler';
+
+import React, { PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect, provide } from 'redux/react';
 import { AuthRequired } from 'modules/auth';
+import redux from 'app-redux';
+import * as TodoActions from 'modules/todo/actions/todo-actions';
+import TodosList from 'modules/todo/components/todos-list';
+import TodosHeader from 'modules/todo/components/todos-header';
 
 @AuthRequired
-class TodoApp extends React.Component {
+@provide(redux)
+@connect(({ todosState }) => ({
+    todos: todosState.todos,
+    fetched: todosState.fetched
+}))
+class TodoHandler extends React.Component {
+
+    static propTypes = {
+        todos: PropTypes.array.isRequired,
+        fetched: PropTypes.bool.isRequired,
+        dispatch: PropTypes.func.isRequired
+    };
+
     render() {
+        const { todos, fetched, dispatch } = this.props;
+        const actions = bindActionCreators(TodoActions, dispatch);
         return (
-          <Provider redux={redux}>
-            {() => <TodoHandler />}
-          </Provider>
+            <div>
+                <TodosHeader actions={actions} />
+                <TodosList todos={todos} fetched={fetched} actions={actions} />
+            </div>
         );
     }
+
 }
 
 export default {
-    TodoHandler: TodoApp
+    TodoHandler: TodoHandler
 };
