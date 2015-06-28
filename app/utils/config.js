3d7e0c48a1106e74514316fb2ec7c987d598d682
Alexander Suevalov <suevalov.work@gmail.com>
2015-06-28 19:19:58 +0300

    Removed using Immutable.js from utils/config.js

--- app/utils/config.js
+++ app/utils/config.js
@@ -1,13 +1,7 @@
-import Immutable from 'immutable';
-
-const config = Immutable.fromJS({
-    apiendpoint: 'http://localhost:5000/api'
-});
-
 module.exports = {
 
     getApiEndpoint(path = '') {
-        return config.get('apiendpoint') + path;
+        return `http://localhost:5000/api/${path}`;
     }
 
 };
