99692cef19a0a9a5de3a8c6ef8680780dcd6361d
Alexander Suevalov <suevalov.work@gmail.com>
2015-07-21 11:31:30 +0300

    Added Sails application

---
+++ sails/api/controllers/TodosController.js
@@ -0,0 +1,11 @@
+/**
+ * TodosController
+ *
+ * @description :: Server-side logic for managing todos
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
